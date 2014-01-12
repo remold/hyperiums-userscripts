5e3940e4ebdb31d64020dc77626edccdcfd97aab
Remold Krol <remold.krol@everett.nl>
2014-01-12 15:58:42 +0100

    Added Forum enhancement It places a link ('=>') in forum thread list to goto last page of thread

---
+++ forum-enhance.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name        Hyperiums forum enhancements
+// @namespace   http://github.com/Nasga/hyperiums-greasemonkey/
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
+// @include     http://hyp2.hyperiums.com/servlet/Forums*
+// @version     17
+// @grant       none
+// ==/UserScript==
+
+/* global $:false */
+
+"use strict";
+
+
+if(window.location.search.indexOf("action=fenter") > -1) {
+    var linkToLast;
+    $('body center form td:not(.hc) a')
+        .each(function(idx, elt) {
+            linkToLast = $(elt).clone(true);
+            linkToLast.prop("href",$(elt).prop("href") + "&gotolast=1");
+            linkToLast.attr("Title","Last page");
+            linkToLast.text('=>');
+            $(elt).after(linkToLast);
+            linkToLast.before(" ");
+        }
+    )
+
+}
+
+if(window.location.search.indexOf("action=fdispmsg") > -1) {
+    if(window.location.search.indexOf("gotolast=1") > -1) {
+        var lastPagelink = "";
+        var currentLink = "";
+//        /html/body/center/center/table[2]/tbody/tr/td/a
+        $('td.hc.info:last a')
+            .each(function(idx, elt) {
+//                if(!elt.hasClass(avgText) && !elt.hasClass(hugeText)) {
+//                alert("current=" + $(elt).prop("href"));
+                    if( currentLink != "") {
+                        lastPagelink = currentLink;
+                    }
+                    currentLink = $(elt).prop("href");
+                }
+//            }
+        )
+        if (lastPagelink != "") {
+//            alert("result=" + lastPagelink);
+            window.location.assign(lastPagelink);
+        }
+
+    }
+}
