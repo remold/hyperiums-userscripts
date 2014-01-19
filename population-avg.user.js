f3387a4ab9966bd363403cc4033773a6238165db
Remold Krol <remold.krol@everett.nl>
2014-01-20 00:37:45 +0100

    add copyright and license

--- population-avg.user.js
+++ population-avg.user.js
@@ -5,6 +5,10 @@
 // @include     http://hyp2.hyperiums.com/servlet/Home
 // @version     34
 // @grant       none
+// @copyright   2013+, Remold Krol (https://github.com/remold)
+// @license     Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+// @author      Remold Krol
+// @homepage    https://github.com/remold
 // ==/UserScript==
 
 /* global $:false */
