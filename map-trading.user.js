68dd0372286ee06eccafda01f6f6fd632b60a766
Remold Krol <remold.krol@everett.nl>
2014-01-21 01:43:23 +0100

    Add version info

--- map-trading.user.js
+++ map-trading.user.js
@@ -9,6 +9,22 @@
 // @grant       none
 // ==/UserScript==
 
+/*
+ ### Revisions
+ v6
+ * Add planet count
+ v5
+ * Fix bhed planets
+ v4
+ * Add civ level
+ v3
+ * Manage planet id for future use
+ v2
+ * Empty tag will now display []
+ * Fix a bug, the center planet was not visible on the map.
+ v1
+ * Initial release
+ */
 $('#searchButton')
   .after(
     '&nbsp;' + 
