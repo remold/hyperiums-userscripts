1bfe88359b6a8f842c2c484fb7dedca5f28c3e5e
Remold Krol <remold.krol@everett.nl>
2014-01-21 01:44:32 +0100

    Made compatible with planet deduction

--- upkeep-percent.user.js
+++ upkeep-percent.user.js
@@ -1,8 +1,15 @@
 // ==UserScript==
 // @name Upkeep percent          
-// @namespace https://github.com/Nasga/hyperiums-greasemonkey
-// @include http://hyp2.hyperiums.com/servlet/Cash*
-// @require http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
+// @namespace   http://github.com/remold/hyperiums-greasemonkey/
+// @include     http://hyp2.hyperiums.com/servlet/Cash*
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
+// @version     1
+// @grant       none
+// @copyright   2012+, Nasga (https://github.com/Nasga) + Remold Krol (https://github.com/remold)
+// @license     Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+// @author      Nasga (https://github.com/Nasga)
+// @contributor Remold Krol (https://github.com/remold)
+// @homepage    https://github.com/remold
 // ==/UserScript==
 
 /*global $:false */
@@ -10,24 +17,36 @@
 "use strict";
 
 var ti = $("td:contains('Total income from controlled planets')")
-  .next("td")
-  .text();
+    .next("td")
+    .text();
+
+var deduct = $("td:contains('Deduction for planets above limit')")
+    .next("td")
+    .text();
 
 var upkeep = $("td:contains('armies upkeep cost')")
-  .next('td')
-  .text();
+    .next('td')
+    .text();
 
 if (upkeep) {
-  ti = parseInt(
-    ti.replace(/,/g,'')
-  );
-  upkeep = parseInt(
-    upkeep.replace(/,/g,'')
-      .replace('-','')
-  );
+    ti = parseInt(
+        ti.replace(/,/g,'')
+    );
+    if (deduct) {
+        deduct = parseInt(
+            deduct.replace(/,/g,'')
+                .replace('-','')
+        );
+        ti = ti - deduct;
 
-  var percent = Math.round(upkeep / ti * 1000) / 10;
+    };
+    upkeep = parseInt(
+        upkeep.replace(/,/g,'')
+            .replace('-','')
+    );
 
-  $("td:contains('armies upkeep cost')")
-    .append(" (" + percent + "%)");
+    var percent = Math.round(upkeep / ti * 1000) / 10;
+
+    $("td:contains('armies upkeep cost')")
+        .append(" (" + percent + "%)");
 }
